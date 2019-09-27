154f25b7b6c2ca4536b6e19520fe4ab262671019
Christian Perez <chrisperez626@gmail.com>
2019-09-27 19:41:59 -0400

    three sum problem

---
+++ threeSum.java
@@ -0,0 +1,36 @@
+public class threeSum {
+
+    static void threeSum(Integer[] array){
+
+        int sum = 0;
+        for(int i = 0; i < array.length - 2; i++) {
+
+            // Fix the second element as A[j]
+            for(int j = i + 1; j < array.length - 1; j++) {
+
+                // Now look for the third number
+                for(int k = j + 1; k < array.length; k++) {
+                    if (array[i] + array[j] + array[k] == sum) {
+                        System.out.print("Triplet is " + A[i] + ", " + A[j] + ", " + A[k]);
+                        return true;
+                    }
+                }x
+            }
+        }
+
+        return false;
+    }
+
+
+
+
+    public static void main(String[] args){
+
+        threeSum triplet = new threeSum();
+        int A[] = { 1, 4, 45, 6, 10, 8 };
+
+
+        triplet.threeSum(A);
+
+    }
+}
