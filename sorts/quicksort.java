a6cb172de6a58a606012a5f5a557d7ba0a25a4d1
Christian Perez <chrisperez626@gmail.com>
2019-11-07 22:35:08 -0500

    add java quick sort

--- sorts/quicksort.java
+++ sorts/quicksort.java
@@ -1,9 +1,25 @@
 public class quicksort {
-    static void quickSort(Integer[] nums){
+    static void quickSort(int[] nums){
 
+
+    }
+    static void partition(int[] nums){
+        int l = 0, hi = nums.length - 1;
+        for(int i = 0; i < nums.length; i++){
+            for(int j = nums.length - 1; j < nums.length; j--){
+                if(nums[i] < nums[j]){
+
+                }
+                if(nums[i] > nums[j]){
+
+                }
+            }
+        }
     }
 
     public static void main(String[] args){
+        int[] arr = {2, 45, 13, 4, 23}
+
         System.out.println(quickSort();)
     }
 }
